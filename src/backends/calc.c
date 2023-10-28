874d1583a635a76192f3536622f535d94e64f7af
Thatsmusic99 <holly.jpacker2@gmail.com>
2023-10-28 16:53:51 +0100

    feat: work on calc and stuff and yada yada

---
+++ src/backends/calc.c
@@ -0,0 +1,68 @@
+
+#include "../sprites/main_sprite.h"
+
+#define LCD_WIDTH_PX 384
+#define LCD_HEIGHT_PX 216
+
+#define SPAWNING_Y 180
+
+#define JUMP 0x0902
+#define LEFT 0x0903
+#define RIGHT 0x0802
+
+extern color_t* main_sprite;
+// extern color_t* grass_block;
+// extern color_t* dirt_block;
+// extern color_t* enemy_sprite;
+
+// char* VRAM = (char*) 0xA8000000;
+
+enum EntityType {
+    Player,
+    Entity
+};
+
+struct Coord {
+    int x, y;
+};
+
+struct Entity {
+    struct Coord pos;
+    float vel, acc;
+    enum EntityType type;
+    const color_t * sprite;
+};
+
+int initDisplay(void * display) {
+    
+    // Draws the main sprite
+    Entity main_sprite = {{10, SPAWNING_Y}, 0, 0, Player, main_sprite};
+    CopySprite(main_sprite.sprite);
+}
+
+void pollEvents(unsigned short * keys) {
+    
+}
+
+void moveEntities(unsigned short key, struct Entity * entities) {
+    
+}
+
+int detectDeath(struct Entity * entities) {
+
+}
+
+void updateDisplay(struct Block * level, struct Entity * entities, void * display) {
+    
+}
+
+void CopySprite(color_t* sprite, int x, int y, int width, int height) {
+    char* VRAM = (char*)0xA8000000;
+    VRAM += LCD_WIDTH_PX*y + x;
+    for(int j=y; j<y+height; j++) {
+        for(int i=x; i<x+width; i++) {
+            *(VRAM++) = *(sprite++);
+        }
+        VRAM += LCD_WIDTH_PX-width;
+    }
+}
--- src/main.c
+++ src/main.c
@@ -2,11 +2,16 @@
 // Created by akp on 28/10/23.
 //
 
+#include "backends/calc.c"
+
 #if defined(BACKEND_CALC)
 int main()
 #elif defined(BACKEND_PC)
 int main(int argc, char** argv)
 #endif
 {
+    initDisplay();
+    while (1) {
+    }
     return 0;
 }
